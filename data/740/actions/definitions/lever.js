7750eac22a095fed56a77b8b70ccdb527a5bc3c8
Pedro Giampietro <pedrogiampietro@hotmail.com>
2025-12-26 18:03:49 -0300

    feat: Add special lever handling with support for toggling bridges and general lever state changes.

--- data/740/actions/definitions/lever.js
+++ data/740/actions/definitions/lever.js
@@ -1,1 +1,9 @@
-module.exports = function() {}
+/**
+ * Lever Action Handler
+ * Delegates to special-lever.js for custom actionId handling
+ */
+const specialLever = require("./special-lever.js");
+
+module.exports = function (player, tile, index, item) {
+    return specialLever(player, tile, index, item);
+};
